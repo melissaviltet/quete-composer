f6e308f493ea4f4f45ec0139d294c1bc1e5fc8e1
Melissa Viltet <melissaviltet@gmail.com>
2018-09-27 13:59:48 +0200

    blabla2

---
+++ src/Wcs/Hello.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder16
+ * Date: 27/09/18
+ * Time: 12:10
+ */
+
+namespace App\Wcs;
+
+
+class Hello
+{
+    public function talk(){
+        return "hello wolrd !";
+    }
+
+
+}
---
+++ src/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require "../vendor/autoload.php";
+use App\Wcs\Hello;
+$hello = new Hello();
+
+echo $hello -> talk ();
+
+
